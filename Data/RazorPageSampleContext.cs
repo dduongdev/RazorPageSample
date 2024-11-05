12a0f72c66ae876f52ba0acb9687bd9f1a6b2eba
dduongdev <nguyendongduong4551190010@gmail.com>
2024-11-05 20:08:07 +0700

    Add project files.

---
+++ Data/RazorPageSampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPageSample.Models;
+
+namespace RazorPageSample.Data
+{
+    public class RazorPageSampleContext : DbContext
+    {
+        public RazorPageSampleContext (DbContextOptions<RazorPageSampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPageSample.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPageSample.Data;
+using RazorPageSample.Models;
+
+namespace RazorPageSample.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPageSample.Data.RazorPageSampleContext _context;
+
+        public CreateModel(RazorPageSample.Data.RazorPageSampleContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
